f1f58cb32603474c18318af261eb9f237d133709
ThiagoLimaJ <thiagodejesus2@gmail.com>
2020-10-08 22:10:25 -0300

    Removendo dados duplicados

--- Aula06-Repetição/Repeticao2.kt
+++
@@ -1,7 +0,0 @@
-fun main(){
-    for (c in 1..50){
-        if (c%2 != 0){
-            println(c)
-        }
-    }
-}
--- Aula06-Repetição/Repeticao3.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    print("Favor digitar primeiro numero: ")
-    var numeroUm = readLine()!!.toInt()
-    print("Favor digitar segundo numero: ")
-    var numeroDois = readLine()!!.toInt()
-    for (c in numeroUm..numeroDois){
-        println(c)
-    }
-}
